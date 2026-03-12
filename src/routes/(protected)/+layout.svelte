d073fbc9bafe16c0b5b908d0153e2e534e4e7906
Thalmis <127175411+thalmis-zt@users.noreply.github.com>
2026-03-12 15:27:39 +0530

    semantics corrected

--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import '../../app.css';
-
+import Header from '$lib/components/Header.svelte';
 
 	import { page } from '$app/stores';
 	// import { sidebarCollapsed } from '$lib/stores/sidebarStore.js';
@@ -20,10 +20,10 @@
 	
 </script>
 
-<div class="flex min-h-screen flex-col px-4 pt-4 pb-24 bg-base">
+<div class="flex min-h-screen flex-col  bg-base">
 
 	{#if showHeader}
-		<!-- space for header -->
+		<Header />
 	{/if}
 
 	<div class="flex flex-1">
@@ -32,7 +32,7 @@
 			<!-- space for sidebar -->
 		{/if}
 
-		<main class="flex-1 flex flex-col">
+		<main class="flex-1 flex flex-col px-4 pt-4 pb-24">
 			{@render children?.()}
 		</main>
 
