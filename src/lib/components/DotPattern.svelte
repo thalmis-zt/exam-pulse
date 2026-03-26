1c1e5da0a67e23fa63d2c19303018b9e49c61669
meera-salim <meera.salim@tensorlogic.ai>
2026-03-26 15:41:16 +0530

    Create DotPattern.svelte

---
+++ src/lib/components/DotPattern.svelte
@@ -0,0 +1,10 @@
+	<div class="absolute inset-0 overflow-hidden rounded-xl">
+		<!-- pattern -->
+		<div
+			class="absolute inset-0 bg-[radial-gradient(circle_at_1px_1px,white_1px,transparent_0)] bg-[size:12px_12px] opacity-10"
+		></div>
+
+		<!-- blobs -->
+		<div class="absolute top-0 right-0 h-32 w-32 rounded-full bg-white/20 blur-xl"></div>
+		<div class="absolute bottom-0 left-0 h-40 w-40 rounded-full bg-white/10 blur-2xl"></div>
+	</div>
