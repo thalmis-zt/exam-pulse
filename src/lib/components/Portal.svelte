08adf1efcca9f7ff5d586fb8988b4e74f3ce3cfa
meera-salim <meera.salim@tensorlogic.ai>
2026-04-02 12:02:43 +0530

    Create Portal.svelte

---
+++ src/lib/components/Portal.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { onMount, onDestroy } from 'svelte';
+
+	let slotContainer;
+	let container;
+
+	onMount(() => {
+		if (typeof window !== 'undefined' && slotContainer) {
+			container = document.querySelector('#modal-root');
+			if (container) {
+				container.appendChild(slotContainer);
+			}
+		}
+	});
+
+	onDestroy(() => {
+		if (container && slotContainer) {
+			container.removeChild(slotContainer);
+		}
+	});
+</script>
+
+<div>
+	<div bind:this={slotContainer} class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity z-[999] flex items-center justify-center">
+			<slot />
+	</div>
+</div>
